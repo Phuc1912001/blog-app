1a367cc78fa3f766b3e544499036bbe6e7b8125b
Phuc1912001 <phucphuc1912001@gmail.com>
2023-06-04 21:32:31 +0700

    signup , login , CRU user , get gobal article

---
+++ src/Redux/store.ts
@@ -0,0 +1,37 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import userSlice from "./feature/userSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
+    user: userSlice,
+  })
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
